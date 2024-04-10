ef8da89c570e056bf855d1d0c75e11eb6d92772f
nishchintmakode <nishchintsmakode@gmail.com>
2024-04-10 04:36:19 +0100

    Added some components and their logic

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ModeToggle } from "./ui/ToogleButton";
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Navbar;
