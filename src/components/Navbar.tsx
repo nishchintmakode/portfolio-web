14752ce386d89d66640dcff83535dcf7496807e9
nishchintmakode <nishchintsmakode@gmail.com>
2024-05-28 09:09:47 +0100

    Changes to Nav layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,27 +71,27 @@ const Navbar = () => {
               <Menu className="text-gray-600 dark:text-gray-400" />
             </Button>
             <div
-              className={`absolute right-0 mt-2 w-40 bg-popover border rounded shadow-lg transform-gpu transition-transform duration-200 ease-in-out ${
+              className={`absolute right-0 mt-2 w-40 px-1 py-1 bg-popover border rounded-md shadow-md transform-gpu transition-transform duration-200 ease-in-out ${
                 menuOpen ? "opacity-100 scale-100" : "opacity-0 scale-0"
               }`}
             >
               <a
                 href="#about"
-                className="block px-3 py-2 text-sm"
+                className="relative flex cursor-default select-none items-center rounded-sm px-2.5 py-1.5 text-sm outline-none hover:bg-accent hover:text-accent-foreground"
                 onClick={handleLinkClick}
               >
                 About
               </a>
               <a
                 href="#certifications"
-                className="block px-3 py-2 text-sm"
+                className="relative flex cursor-default select-none items-center rounded-sm px-2.5 py-1.5 text-sm outline-none bg-background hover:bg-accent hover:text-accent-foreground"
                 onClick={handleLinkClick}
               >
                 Certifications
               </a>
               <a
                 href="#projects"
-                className="block px-3 py-2 text-sm"
+                className="relative flex cursor-default select-none items-center rounded-sm px-2.5 py-1.5 text-sm outline-none bg-background hover:bg-accent hover:text-accent-foreground"
                 onClick={handleLinkClick}
               >
                 Projects
